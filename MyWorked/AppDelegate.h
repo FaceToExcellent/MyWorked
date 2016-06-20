3ecb22228d2f0f6aaac67b87a168e7b03afd303b
huamanlou <15810427253@163.com>
2016-06-20 18:25:39 +0800

    Initial Commit

---
+++ MyWorked/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWorked
+//
+//  Created by HYZ on 16/6/20.
+//  Copyright © 2016年 HYZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyWorked/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyWorked
+//
+//  Created by HYZ on 16/6/20.
+//  Copyright © 2016年 HYZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
