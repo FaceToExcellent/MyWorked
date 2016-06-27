31c433ef517ccdbd92c9a42e92733954212299f0
huamanlou <15810427253@163.com>
2016-06-27 10:45:56 +0800

    1

--- MyWorked/TextContainerViewController.m
+++ MyWorked/TextContainerViewController.m
@@ -4,22 +4,12 @@
 //
 //  Created by HYZ on 16/6/24.
 //  Copyright © 2016年 HYZ. All rights reserved.
-//
-#define kExamTextFieldWidth 80
-#define kExamTextFieldHeight 20
-#define kAttrLabelWidth (CGRectGetWidth(self.view.frame)-20)
-#define kTextFieldTag 1000
 
 #import "TextContainerViewController.h"
 #import "TYAttributedLabel.h"
-#import "RegexKitLite.h"
-#import "TExamTextField.h"
-#import "TPKeyboardAvoidingScrollView.h"
-@interface TextContainerViewController ()
-@property (nonatomic,strong) TYAttributedLabel *label;
-@property (nonatomic,strong) TYTextContainer *textContainer;
-@property (nonatomic,strong) NSAttributedString *attString;
-@property (nonatomic,weak) TPKeyboardAvoidingScrollView *scrollView;
+#import "TYTextStorageParser.h"
+@interface TextContainerViewController ()<TYAttributedLabelDelegate>
+@property (nonatomic, weak) TYAttributedLabel *label;
 
 @property (nonatomic,strong) NSArray *answerArray;
 @end
@@ -28,30 +18,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-     _answerArray = @[@"白露为霜",@"所谓伊人",@"道阻且跻",@"蒹葭采采",@"白露未已",@"宛在水中沚",@"企慕"];
+  self.title=@"没写containner";
+    // appendAttributedText
+    [self addTextAttributedLabel];
+    
+    // jsonPath
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"content" ofType:@"json"];
+    
+    // parseJson
+    [self parseJsonFileWithPath:path];
+    
+    [_label sizeToFit];
+    
+}
+
+- (void)addTextAttributedLabel
+{
+    TYAttributedLabel *label = [[TYAttributedLabel alloc]initWithFrame:CGRectMake(0, 64, CGRectGetWidth(self.view.frame), 0)];
+    label.delegate = self;
+    [self.view addSubview:label];
+    _label = label;
+}
+
+- (void)parseJsonFileWithPath:(NSString *)filePath
+{
+    NSArray *textStorageArray = [TYTextStorageParser parseWithJsonFilePath:filePath];
+    
+    if (textStorageArray.count > 0) {
+        [_label appendTextStorageArray:textStorageArray];
+    }
+}
+
+#pragma mark - TYAttributedLabelDelegate
+
+- (void)attributedLabel:(TYAttributedLabel *)attributedLabel textStorageClicked:(id<TYTextStorageProtocol>)TextStorage atPoint:(CGPoint)point
+{
+    NSLog(@"textStorageClickedAtPoint");
+    if ([TextStorage isKindOfClass:[TYLinkTextStorage class]]) {
+        NSString *linkStr = ((TYLinkTextStorage*)TextStorage).linkData;
+        
+        if ([linkStr hasPrefix:@"http"]) {
+            [ [ UIApplication sharedApplication] openURL:[ NSURL URLWithString:linkStr]];
+        }else {
+            UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"点击提示" message:linkStr delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
+            [alertView show];
+        }
+    }
+}
+
+- (void)attributedLabel:(TYAttributedLabel *)attributedLabel textStorageLongPressed:(id<TYTextStorageProtocol>)textStorage onState:(UIGestureRecognizerState)state atPoint:(CGPoint)point
+{
+    NSLog(@"textStorageLongPressed");
 }
 
 
 
-
-
-
-
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)makeUI
+{
+    
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
