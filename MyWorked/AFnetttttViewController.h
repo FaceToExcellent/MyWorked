3bcc480320c941c4deda3f5611a13c965655047e
huamanlou <15810427253@163.com>
2016-07-06 16:07:00 +0800

    网路 就不写了

---
+++ MyWorked/AFnetttttViewController.h
@@ -0,0 +1,13 @@
+//
+//  AFnetttttViewController.h
+//  MyWorked
+//
+//  Created by HYZ on 16/7/6.
+//  Copyright © 2016年 HYZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFnetttttViewController : UIViewController
+
+@end
---
+++ MyWorked/AFnetttttViewController.m
@@ -0,0 +1,38 @@
+//
+//  AFnetttttViewController.m
+//  MyWorked
+//
+//  Created by HYZ on 16/7/6.
+//  Copyright © 2016年 HYZ. All rights reserved.
+//
+
+#import "AFnetttttViewController.h"
+//#import "AFNetworking.h"
+@interface AFnetttttViewController ()
+
+@end
+
+@implementation AFnetttttViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title=@"AFNetworking";
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
